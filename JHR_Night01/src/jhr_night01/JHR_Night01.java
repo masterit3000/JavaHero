4049d6117df749683b8046ededa2d98c952ce127
Dang Tuan Tu Tu <masterit3000@gmail.com>
2018-12-17 21:59:35 +0700

    aa

---
+++ JHR_Night01/src/jhr_night01/JHR_Night01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jhr_night01;
+
+import com.demo.annnn.Dog;
+
+/**
+ *
+ * @author Admin
+ */
+public class JHR_Night01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dog dog1;//khai bao
+        dog1 = new Dog();//khoi tao
+        //obj muon dung dc, buoc phai khoi tao
+        
+    }
+    
+}
